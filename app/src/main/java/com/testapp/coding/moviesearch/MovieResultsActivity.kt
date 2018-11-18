6b9d4b95fcd4bd91318fa761ce99865e1daec80a
Raj Kumar Rapolu <rajrapolu@gmail.com>
2018-11-18 17:49:08 -0600

    Initial commit

---
+++ app/src/main/java/com/testapp/coding/moviesearch/MovieResultsActivity.kt
@@ -0,0 +1,12 @@
+package com.testapp.coding.moviesearch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieResultsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_results)
+    }
+}
